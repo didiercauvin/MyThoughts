02e33c9cf94c2a2ed3c3ceba98548141f12655fc
didiercauvin <didier.cauvin@outlook.fr>
2018-10-05 16:02:19 +0200

    Suppression repertoire MyLinks

--- src/MyLinks/Link/Model.elm
+++
@@ -1,8 +0,0 @@
-module MyLinks.Link.Model exposing (..)
-
-type alias Model =
-    {   link : Link
-    ,   errors : List String
-    }
-
-type alias Link = String
--- src/MyLinks/LinkList/Model.elm
+++
@@ -1,5 +0,0 @@
-module MyLinks.LinkList.Model exposing (..)
-
-import MyLinks.Link.Model exposing (..)
-
-type alias Model = List Link
